cf54c94175ff6977c834d0438b7a6e440f0c28d3
Vasyuchenko Alexander <alex-v@macsoftex.com>
2014-12-15 14:54:02 +0300

    added function Url.addShortHost

--- src/main/java/com/macsoftex/android_tools/Url.java
+++ src/main/java/com/macsoftex/android_tools/Url.java
@@ -15,6 +15,8 @@ import java.net.URLEncoder;
  */
 public class Url
 {
+    private static final String[] LEVEL_1_DOMAINS = {"biz", "com", "edu", "gov", "info", "jobs", "mobi", "name", "net", "org", "pro", "tel", "xxx", "ac", "ad", "ae", "af", "ag", "ai", "al", "am", "an", "ao", "aq", "ar", "as", "at", "au", "aw", "ax", "az", "ba", "bb", "bd", "be", "bf", "bg", "bh", "bi", "bj", "bm", "bn", "bo", "br", "bs", "bt", "bv", "bw", "by", "bz", "ca", "cc", "cd", "cf", "cg", "ch", "ci", "ck", "cl", "cm", "cn", "co", "cr", "cs", "cu", "cv", "cx", "cy", "cz", "de", "dj", "dk", "dm", "do", "dz", "ec", "ee", "eg", "eh", "er", "es", "et", "eu", "fi", "fj", "fk", "fm", "fo", "fr", "ga", "gb", "gd", "ge", "gf", "gg", "gh", "gi", "gl", "gm", "gn", "gp", "gq", "gr", "gs", "gt", "gu", "gw", "gy", "hk", "hm", "hn", "hr", "ht", "hu", "id", "ie", "il", "im", "in", "io", "iq", "ir", "is", "it", "je", "jm", "jo", "jp", "ke", "kg", "kh", "ki", "km", "kn", "kp", "kr", "kw", "ky", "kz", "la", "lb", "lc", "li", "lk", "lr", "ls", "lt", "lu", "lv", "ly", "ma", "mc", "md", "mg", "mh", "mk", "ml", "mm", "mn", "mo", "mp", "mq", "mr", "ms", "mt", "mu", "mv", "mw", "mx", "my", "mz", "na", "nc", "ne", "nf", "ng", "ni", "nl", "no", "np", "nr", "nu", "nz", "om", "pa", "pe", "pf", "pg", "ph", "pk", "pl", "pm", "pn", "pr", "ps", "pt", "pw", "py", "qa", "re", "ro", "ru", "rw", "sa", "sb", "sc", "sd", "se", "sg", "sh", "si", "sj", "sk", "sl", "sm", "sn", "so", "sr", "st", "su", "sv", "sy", "sz", "tc", "td", "tf", "tg", "th", "tj", "tk", "tl", "tm", "tn", "to", "tp", "tr", "tt", "tv", "tw", "tz", "ua", "ug", "uk", "um", "us", "uy", "uz", "va", "vc", "ve", "vg", "vi", "vn", "vu", "wf", "ws", "ye", "yt", "yu", "za", "zm", "zw"};
+
     public static String getFileNameFromUrl(final String url)
     {
         Uri uri = Uri.parse( url );
@@ -151,5 +153,55 @@ public class Url
         }
 
         return "http://www.google.by/search?q=" + query;
+    }
+
+    public static String getShortHost(String url)
+    {
+        String host = Uri.parse( url ).getHost().toLowerCase();
+        String[] parts = host.split("\\.");
+        final int n = parts.length;
+
+        if (n < 2)
+            return null;
+
+        String lastPart = parts[n-1];
+        boolean isInt;
+
+        try
+        {
+            Integer.parseInt( lastPart );
+            isInt = true;
+        }
+        catch(NumberFormatException nfe)
+        {
+            isInt = false;
+        }
+
+        if (isInt && n>=4)
+        {
+            return host;
+        }
+        else
+        {
+            String level2 = parts[n-2];
+            boolean contains = false;
+
+            for (String domain : LEVEL_1_DOMAINS)
+                if ( level2.equals( domain ) )
+                {
+                    contains = true;
+                    break;
+                }
+
+            if (contains && n>=3)
+            {
+                return String.format("%s.%s.%s", parts[n-3], parts[n-2], parts[n-1]);
+            }
+            else
+            {
+                return String.format("%s.%s", parts[n-2], parts[n-1]);
+            }
+        }
+
     }
 }
