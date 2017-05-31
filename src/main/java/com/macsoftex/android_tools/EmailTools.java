1c3c85030fd27119af7cb3c548272882a6567783
Alexander Vasyuchenko <alex-v@macsoftex.com>
2017-05-31 16:37:58 +0300

    no message

--- src/main/java/com/macsoftex/android_tools/EmailTools.java
+++ src/main/java/com/macsoftex/android_tools/EmailTools.java
@@ -1,17 +1,19 @@
 package com.macsoftex.android_tools;
 
-import android.content.ActivityNotFoundException;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ResolveInfo;
 import android.net.Uri;
 
-import java.util.ArrayList;
+import java.io.File;
+import java.util.List;
 
 /**
  * Created by alex-v on 23.03.16.
  */
 public class EmailTools {
-    public static boolean sendEmail(Context context, String email, String subject, String body, ArrayList<Uri> attachmentFiles) {
+    public static boolean sendEmail(Context context, String email, String subject, String body, File attachmentFile) {
         try {
             Intent intent = new Intent(Intent.ACTION_SENDTO);
             intent.setType("message/rfc822");
@@ -20,13 +22,24 @@ public class EmailTools {
             intent.putExtra(Intent.EXTRA_SUBJECT, subject);
             intent.putExtra(Intent.EXTRA_TEXT, body);
 
-            if (attachmentFiles != null)
-                intent.putParcelableArrayListExtra(Intent.EXTRA_STREAM, attachmentFiles);
+            if (attachmentFile != null)
+                intent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(attachmentFile));
+
+            List<ResolveInfo> resolveInfos = context.getPackageManager().queryIntentActivities(intent, 0);
+
+            if (resolveInfos.size() == 0)
+                return false;
+
+            String packageName = resolveInfos.get(0).activityInfo.packageName;
+            String name = resolveInfos.get(0).activityInfo.name;
+
+            intent.setAction(Intent.ACTION_SEND);
+            intent.setComponent(new ComponentName(packageName, name));
 
             context.startActivity(intent);
 
             return true;
-        } catch (ActivityNotFoundException e) {
+        } catch (Exception e) {
             e.printStackTrace();
             return false;
         }
